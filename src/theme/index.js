3348086200430b3d2558bbfa500347d42d9c3be3
Anthony Steven Quito Penadillo <anthony.quito@tecsup.edu.pe>
2021-11-14 00:36:27 -0500

    first commit

---
+++ src/theme/index.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette:{
+        primary:{
+            light:"#007c91",
+            main:"#00acc1",
+            dark:"#5ddef4"
+        },
+        secondary: {
+            light:"#ffd95a",
+            main:"#f9a825",
+            dark:"#c17900"
+        },
+    },
+});
+
+export default theme;
